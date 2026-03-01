0cb1cdc631cbed6bddba7f30a5177e9d51c476e5
Icosahunter <anzelknight@gmail.com>
2026-03-01 15:44:45 -0600

    Proof of concept working

---
+++ src/VisuALS_Windows/Applets/Camera/Camera.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+
+namespace VisuALS_WPF_App
+{
+    public class Camera : Applet
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Camera() : base("ap_camera", "ap_desc_camera")
+        {
+            IconSource = "Resources/Images/camera.png";
+            Role = ButtonRole.Notes;
+        }
+
+        /// <summary>
+        /// Returns the main page
+        /// </summary>
+        /// <returns></returns>
+        public override AppletPage CreateMainPage()
+        {
+            return new CameraPage();
+        }
+
+        public override AppletPage CreateSettingsPage()
+        {
+            return new NotepadSettings();
+        }
+
+        /// <summary>
+        /// Initialize config and settings
+        /// </summary>
+        public override void InitializeSettingsValues()
+        {
+
+        }
+    }
+}
---
+++ src/VisuALS_Windows/Applets/Camera/CameraPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Windows.Media.Capture;
+using Windows.Media.Devices;
+using Windows.Storage;
+using Windows.UI.Xaml.Controls;
+
+namespace VisuALS_WPF_App
+{
+    /// <summary>
+    /// Interaction logic for Camera.xaml
+    /// </summary>
+    public partial class CameraPage : AppletPage
+    {
+        
+        PeriodicBackgroundProcess captureWorker;
+        CaptureElement captureElement;
+        MediaCapture capture;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public CameraPage()
+        {
+            InitializeComponent();
+            ParentApplet = AppletManager.GetApplet<Camera>();
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            captureElement = new CaptureElement();
+            capture = new MediaCapture();
+            await capture.InitializeAsync();
+            captureElement.Source = capture;
+            cameraPreviewHost.Child = captureElement;
+            await capture.StartPreviewAsync();
+        }
+
+        private async void CaptureBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            StorageFolder cameraRoll = await StorageFolder.GetFolderFromPathAsync(Path.Combine(AppPaths.PicturesPath, "Camera Roll"));
+            StorageFile file = await cameraRoll.CreateFileAsync($"{DateTime.Now:yyyyMMddTHHmmss}.jpg");
+            await capture.CapturePhotoToStorageFileAsync(Windows.Media.MediaProperties.ImageEncodingProperties.CreateJpeg(), file);
+            captureElement.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            await Task.Delay(250);
+            captureElement.Visibility = Windows.UI.Xaml.Visibility.Visible;
+        }
+    }
+}
